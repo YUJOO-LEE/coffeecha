9603813d9ebfddd83e4d2a39f223522980d70823
YUJOO-LEE <lllllllllee@gmail.com>
2023-08-11 20:45:01 +0900

    feat: api 연결 설정

---
+++ src/apis/index.ts
@@ -0,0 +1,17 @@
+import { CategoryController } from '@/apis/swagger/CategoryController';
+import { Client } from '@/apis/swagger/Client';
+import { HttpClient } from '@/apis/swagger/http-client';
+import { User } from '@/apis/swagger/User';
+import { UserMenu } from '@/apis/swagger/UserMenu';
+
+const axiosConfig = {
+  baseURL: process.env.REACT_APP_API_ROOT,
+  headers: {},
+};
+
+export const http = new HttpClient(axiosConfig);
+
+export const clientApi = new Client(http);
+export const categoryApi = new CategoryController(http);
+export const userApi = new User(http);
+export const userMenuApi = new UserMenu(http);
