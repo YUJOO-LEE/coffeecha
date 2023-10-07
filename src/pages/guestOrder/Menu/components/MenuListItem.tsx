83c282b502b9691911b4c2e2c2d26ec21735f923
YUJOO-LEE <lllllllllee@gmail.com>
2023-10-07 21:52:10 +0900

    style: menu item 해상도 별 노출방식 수정

--- src/pages/guestOrder/Menu/components/MenuListItem.tsx
+++ src/pages/guestOrder/Menu/components/MenuListItem.tsx
@@ -1,13 +1,15 @@
 import { Box, styled, Typography } from '@mui/material';
 import React from 'react';
 
+const maxWidth = 420;
+
 const MenuListItem = (): React.ReactNode => {
   return (
     <Styled.Wrapper>
       <Styled.ImageWrapper>
         <img src="img" alt="title" />
       </Styled.ImageWrapper>
-      <Typography fontSize="16px" fontWeight="500" align="center">
+      <Typography fontSize="16px" fontWeight="500">
         title
       </Typography>
     </Styled.Wrapper>
@@ -20,9 +22,15 @@ const Styled = {
   Wrapper: styled(Box)(({ theme }) => ({
     padding: '16px',
     display: 'grid',
+    gridTemplateColumns: '1fr',
+    justifyItems: 'center',
     gap: '16px',
     borderRadius: '16px',
     backgroundColor: theme.palette.background.default,
+    [`@media screen and (max-width: ${maxWidth}px)`]: {
+      gridTemplateColumns: '56px 1fr',
+      justifyItems: 'flex-start',
+    },
   })),
   ImageWrapper: styled(Box)({
     width: '100%',
