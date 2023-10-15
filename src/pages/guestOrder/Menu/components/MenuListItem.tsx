0fab836cf14e40753c0d3e1b8a95707fa8f4a694
YUJOO-LEE <lllllllllee@gmail.com>
2023-10-15 23:11:13 +0900

    feat: sold out 표시

--- src/pages/guestOrder/Menu/components/MenuListItem.tsx
+++ src/pages/guestOrder/Menu/components/MenuListItem.tsx
@@ -1,4 +1,5 @@
 import { ClientMenuResponse } from '@/apis/swagger/data-contracts';
+import { DoDisturbRounded } from '@mui/icons-material';
 import { Box, styled, Typography } from '@mui/material';
 import React from 'react';
 
@@ -12,14 +13,28 @@ interface Props {
 const MenuListItem = (props: Props): React.ReactNode => {
   const { data, onAddCart } = props;
 
+  const isSoldOut = data.stockQuantity <= data.saleQuantity;
+
   return (
-    <Styled.Wrapper onClick={onAddCart}>
+    <Styled.Wrapper
+      onClick={isSoldOut ? undefined : onAddCart}
+      sx={{ cursor: isSoldOut ? 'default' : 'pointer'}}
+    >
       <Styled.ImageWrapper>
         <img src={data.menuImageUrl} alt="title" />
       </Styled.ImageWrapper>
       <Typography fontSize="16px" fontWeight="500">
         {data.menuName}
       </Typography>
+
+      {isSoldOut && (
+        <Styled.HiddenWrapper>
+          <Styled.HiddenIcon />
+          <Typography fontSize="16px" fontWeight="500" color={(theme) => theme.palette.primary.main}>
+            Sold out
+          </Typography>
+        </Styled.HiddenWrapper>
+      )}
     </Styled.Wrapper>
   );
 };
@@ -28,6 +43,7 @@ export default MenuListItem;
 
 const Styled = {
   Wrapper: styled(Box)(({ theme }) => ({
+    position: 'relative',
     padding: '16px',
     display: 'grid',
     gridTemplateColumns: '1fr',
@@ -35,6 +51,7 @@ const Styled = {
     gap: '16px',
     borderRadius: '16px',
     backgroundColor: theme.palette.background.default,
+    overflow: 'hidden',
     cursor: 'pointer',
     [`@media screen and (max-width: ${maxWidth}px)`]: {
       gridTemplateColumns: '56px 1fr',
@@ -52,4 +69,25 @@ const Styled = {
       objectFit: 'cover',
     },
   }),
+  HiddenWrapper: styled('div')({
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '8px',
+    backdropFilter: 'blur(5px)',
+    backgroundColor: 'rgba(0,0,0,0.15)',
+  }),
+  HiddenIcon: styled(DoDisturbRounded)(({ theme }) => ({
+    '& path': {
+      fill: theme.palette.primary.main,
+    },
+  })),
 };
