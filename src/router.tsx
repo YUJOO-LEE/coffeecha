e55c34bb8635a1301d058ece17691a2f11788e09
YUJOO-LEE <lllllllllee@gmail.com>
2023-11-29 22:21:46 +0900

    refactor: lazy load 컴포넌트 축소

--- src/router.tsx
+++ src/router.tsx
@@ -1,114 +1,108 @@
 import LoadingCircleProgress from '@/components/LoadingCircleProgress';
-import {
-  ClientSettingsPage,
-  CollectionPage,
-  GuestOrderDetailPage,
-  GuestOrderListPage,
-  GuestOrderPage,
-  HomePage,
-  Layout,
-  LoginPage,
-  MenuPage,
-  NewOrderPage,
-  NoCoffeechaDataPage,
-  OrderPage,
-  SalesManagementPage,
-  UserSettingsPage,
-} from '@/pages';
+import { GuestOrderDetailPage, GuestOrderListPage, GuestOrderPage, Layout } from '@/pages';
+import { CollectionPage } from '@/pages/collection';
+import { NoCoffeechaDataPage } from '@/pages/guestOrder/order/Error/NoCoffeechaDataPage';
+import { LoginPage } from '@/pages/login';
+import { SalesManagementPage } from '@/pages/salesManagement';
+import { HomePage } from '@/pages/salesManagement/home';
+import { MenuPage } from '@/pages/salesManagement/menu';
+import { NewOrderPage } from '@/pages/salesManagement/newOrder';
+import { OrderPage } from '@/pages/salesManagement/order';
+import { ClientSettingsPage } from '@/pages/salesManagement/settings';
+import { UserSettingsPage } from '@/pages/settings';
 import React, { Suspense } from 'react';
 import { createBrowserRouter, Outlet, redirect } from 'react-router-dom';
 
 const routers = createBrowserRouter([
   {
+    path: '/',
+    loader: async () => {
+      const isLogin = () => {
+        return localStorage.getItem('auth') !== null;
+      };
+
+      return !isLogin() && redirect('/login');
+    },
     element: (
       <Suspense fallback={<LoadingCircleProgress open={true} />}>
-        <Outlet />
+        <Layout />
       </Suspense>
     ),
     children: [
       {
-        path: '/',
-        loader: async () => {
-          const isLogin = () => {
-            return localStorage.getItem('auth') !== null;
-          };
-
-          return !isLogin() && redirect('/login');
-        },
-        element: <Layout />,
+        index: true,
+        element: <SalesManagementPage />,
+      },
+      {
+        path: 'collection',
+        element: <CollectionPage />,
+      },
+      {
+        path: 'settings',
+        element: <UserSettingsPage />,
+      },
+      {
+        path: ':clientId',
+        element: <Outlet />,
         children: [
           {
             index: true,
-            element: <SalesManagementPage />,
+            element: <HomePage />,
           },
           {
-            path: 'collection',
-            element: <CollectionPage />,
+            path: 'order',
+            element: <OrderPage />,
+          },
+          {
+            path: 'order/new',
+            element: <NewOrderPage />,
+          },
+          {
+            path: 'menu',
+            element: <MenuPage />,
           },
           {
             path: 'settings',
-            element: <UserSettingsPage />,
-          },
-          {
-            path: ':clientId',
-            element: <Outlet />,
-            children: [
-              {
-                index: true,
-                element: <HomePage />,
-              },
-              {
-                path: 'order',
-                element: <OrderPage />,
-              },
-              {
-                path: 'order/new',
-                element: <NewOrderPage />,
-              },
-              {
-                path: 'menu',
-                element: <MenuPage />,
-              },
-              {
-                path: 'settings',
-                element: <ClientSettingsPage />,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: '/login',
-        element: <LoginPage />,
-        loader: async () => {
-          const isLogin = () => {
-            return localStorage.getItem('auth') !== null;
-          };
-
-          return isLogin() && redirect('/');
-        },
-      },
-      {
-        path: '/order',
-        element: <Outlet />,
-        errorElement: <NoCoffeechaDataPage />,
-        children: [
-          {
-            path: 'list',
-            element: <GuestOrderListPage />,
-          },
-          {
-            path: 'detail/:orderKey',
-            element: <GuestOrderDetailPage />,
-          },
-          {
-            path: ':clientKey?',
-            element: <GuestOrderPage />,
+            element: <ClientSettingsPage />,
           },
         ],
       },
     ],
-  }
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+    loader: async () => {
+      const isLogin = () => {
+        return localStorage.getItem('auth') !== null;
+      };
+
+      return isLogin() && redirect('/');
+    },
+  },
+  {
+    path: '/order',
+    element: (
+      <Suspense fallback={<NoCoffeechaDataPage />}>
+        <Outlet />
+      </Suspense>
+    ),
+    errorElement: <NoCoffeechaDataPage />,
+    children: [
+      {
+        path: 'list',
+        element: <GuestOrderListPage />,
+      },
+      {
+        path: 'detail/:orderKey',
+        element: <GuestOrderDetailPage />,
+      },
+      {
+        path: ':clientKey?',
+        element: <GuestOrderPage />,
+      },
+    ],
+  },
 ]);
 
 export default routers;
